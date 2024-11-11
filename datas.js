1dcebd10fe6bf31e5f3fef6672c33317decaafe4
Guilherme <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-11-11 17:44:44 -0300

    Update datas.js

--- datas.js
+++ datas.js
@@ -1,31 +1,7 @@
-const imagensDeFundo = [
-    'url("images/slide1.png")',
-    'url("images/slide2.png")',
-    'url("images/slide3.png")' 
-];
+let user = document.getElementById("user");
+let password = document.getElementById("password");
 
-function alterarFundo()
+if(user == "admin" && password == "1234567")
 {
-    const alturaTotal = document.documentElement.scrollHeight - window.innerHeight;
-    const posicaoRolagem = window.scrollY;
-    let indiceImagem = Math.floor((posicaoRolagem / alturaTotal) * imagensDeFundo.length);
-    indiceImagem = Math.min(indiceImagem, imagensDeFundo.length - 1);
-    let slidepage = document.getElementById("images");
-    if (slidepage)
-        {
-        slidepage.style.backgroundImage = imagensDeFundo[indiceImagem];
-        slidepage.style.backgroundSize = "cover"; 
-        slidepage.style.transition = "background-image 0.5s ease-in-out";
-        if (indiceImagem === 1)
-            {
-            slidepage.style.marginLeft = "0vh";
-            slidepage.style.transition = "1.5s ease";
-        }
-        else
-        {
-            slidepage.style.marginLeft = "5vh";
-            slidepage.style.transition = "1.5s ease";
-        }
-    }
+    window.open("store.html");
 }
-window.addEventListener('scroll', alterarFundo);
