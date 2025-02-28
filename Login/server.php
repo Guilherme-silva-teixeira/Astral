f281eae10ce9b3091e104aac8e696bde6d787b46
Guilherme Teixeira <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2025-02-28 10:04:20 -0300

    Add files via upload

---
+++ Login/server.php
@@ -0,0 +1,13 @@
+<?php
+    if($_SERVER['REQUEST_METHOD']=="POST")
+    {
+        $Email = $_POST["EMAIL_USER_NAME"];
+        $Password = $_POST["USER_PASSWORD"];
+        $echoBoxEmail = $_POST["isNullEmail"];
+
+        if(isset($Email))
+        {
+
+        }
+    }
+?>
